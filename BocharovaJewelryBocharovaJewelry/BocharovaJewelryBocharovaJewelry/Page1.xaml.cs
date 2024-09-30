17e23abb987a9f5714bc962e6b19c7ecb0517afa
j0pka <jo.pka@icloud.com>
2024-09-30 22:51:43 +0500

    вывод информации и картинок

--- BocharovaJewelryBocharovaJewelry/BocharovaJewelryBocharovaJewelry/Page1.xaml.cs
+++ BocharovaJewelryBocharovaJewelry/BocharovaJewelryBocharovaJewelry/Page1.xaml.cs
@@ -23,6 +23,9 @@ namespace BocharovaJewelryBocharovaJewelry
         public Page1()
         {
             InitializeComponent();
+            
+            var currenDecor = Bocharova_JewelryEntities.GetContext().Product.ToList();
+            ProductListView.ItemsSource = currenDecor;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
